0e33619b75bf134e2b8a77e246c5a46fb65da6c8
Jesse Antoszyk <jcantosz@users.noreply.github.com>
2017-04-10 20:34:48 -0400

    Update install_ansible.sh

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -7,7 +7,7 @@ sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
 
 # Boto needed for s3 & elastic-beanstalk
 echo "pip install pycrypto pyyaml boto ansible --quiet"
-sudo pip install pycrypto pyyaml boto ansible --quiet
+sudo pip install pycrypto pyyaml boto boto3 botocore ansible --quiet
 
 echo "apt-get -qq clean"
 sudo apt-get -qq clean
